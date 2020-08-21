344c46f7379659c3b60325fc07e8da110abf04f2
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-08-21 14:02:23 +0300

    Emit comment creation event

--- comments/index.js
+++ comments/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(comments);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -22,6 +23,16 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({ id, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(commentsByPostId[req.params.id]);
 });
 

1e47bb463f9b9fe3a04890587a441509cf078fdf
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-08-25 13:32:17 +0300

    Handle comment moderation

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id, content });
+  comments.push({ id, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -29,18 +29,41 @@ app.post('/posts/:id/comments', async (req, res) => {
     data: {
       id,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   });
 
   res.status(201).send(commentsByPostId[req.params.id]);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event received:', req.body.type);
+
+  const { type, data } = req.body;
+  
+  if (type === 'CommentModerated') {
+    const { id, postId, content, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        content,
+        status
+      }
+    });
+  }
+
   res.send({});
 });
 
 app.listen(4001, () => {
-    console.log('Listening on port 4001');
+  console.log('Listening on port 4001');
 });

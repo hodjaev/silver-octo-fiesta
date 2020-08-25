78abf3613552a492b05d55e653aeea9df93f54a5
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-08-25 14:49:19 +0300

    Implement event sync

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -8,25 +9,17 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvents = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
-    posts[id] = { id, title, comments: []};
+    posts[id] = { id, title, comments: [] };
   }
-
-  if (type === 'CommentCreated') {
+  else if (type === 'CommentCreated') {
     const { id, content, postId, status } = data;
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
-
-  if (type === 'CommentUpdated') {
+  else if (type === 'CommentUpdated') {
     const { id, postId, content, status } = data;
     const post = posts[postId];
     const comment = post.comments.find(comment => {
@@ -36,12 +29,24 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
-  console.log(posts);
-  
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvents(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on port 4002');
+  const result = await axios.get('http://localhost:4005/events');
+
+  for (let event of result.data) {
+    console.log('Processing event:', event.type);
+    handleEvents(event.type, event.data);
+  }
 });

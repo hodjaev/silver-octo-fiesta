cc2742a81568a4226b2b433dd474bed8d0d293a7
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-08-29 12:05:24 +0300

    Update service addresses

--- query/index.js
+++ query/index.js
@@ -43,7 +43,7 @@ app.post('/events', (req, res) => {
 
 app.listen(4002, async () => {
   console.log('Listening on port 4002');
-  const result = await axios.get('http://localhost:4005/events');
+  const result = await axios.get('http://event-bus-srv:4005/events');
 
   for (let event of result.data) {
     console.log('Processing event:', event.type);
